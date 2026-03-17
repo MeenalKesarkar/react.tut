2045874629a512a5219f82a11a5b4971a4c7b7a3
Meenal Kesarkar <159876692+MeenalKesarkar@users.noreply.github.com>
2026-03-17 14:02:19 +0530

    Split header into  a component

--- e-com/src/pages/OrdersPage.jsx
+++ e-com/src/pages/OrdersPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import './header.css';
+import { Header } from '../components/Header'
 import './OrdersPage.css';
 
 export const OrdersPage = () => {
   return (
     <>
     <title>Orders</title>
+
+    <Header />
 
     <div className="header">
         <div className="left-section">
